c89f04e49450ef4c871dbbba4157d0e1bf821cf9
Javier Quiroga <javier.quiroga.e@gmail.com>
2023-01-07 14:35:53 -0500

    show board

--- app.py
+++ app.py
@@ -5,8 +5,21 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "abc123"
-app.debug = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.debug = False
 
 toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+
+@app.route("/")
+def root():
+    """root."""
+    return redirect("/game")
+
+@app.route("/game")
+def show_game():
+    """Game."""
+    board = boggle_game.make_board()
+    return render_template("game.html",board=board)
